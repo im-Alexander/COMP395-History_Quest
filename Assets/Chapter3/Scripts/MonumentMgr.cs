7b63be24a9c5540a83896a67d58d388537b55990
asikalev <300783815@cencol.ca>
2018-04-17 12:10:35 -0400

    Classwork Apr.17

---
+++ Assets/Chapter3/Scripts/MonumentMgr.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MonumentMgr : MonoBehaviour {
+	List<GameObject> mountPoints;
+
+
+	public void attachObjToMountPoint(GameObject go, int index){
+		GameObject newGO = Instantiate (go
+			, mountPoints [index].transform.position
+			, mountPoints [index].transform.rotation);
+
+		newGO.SetActive (true);
+		newGO.transform.parent = mountPoints [index].transform;
+		newGO.transform.localPosition = Vector3.zero;
+		newGO.transform.localEulerAngles = Vector3.zero;
+	}
+	// Use this for initialization
+	void Start () {
+
+	}
+
+	// Update is called once per frame
+	void Update () {
+
+	}
+}
